a6db88f5d2906ebc6a67ae8d8d6c7018e24393c8
ColbertCoinProject <therealcolbertcoin@gmail.com>
2014-02-15 14:20:35 -0800

    changes

--- litecoin/src/qt/bitcoinunits.cpp
+++ litecoin/src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("COL");
+    case mBTC: return QString("mCOL");
+    case uBTC: return QString::fromUtf8("μCOL");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("ColbertCoins");
+    case mBTC: return QString("Milli-ColbertCoins (1 / 1,000)");
+    case uBTC: return QString("Micro-ColbertCoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
